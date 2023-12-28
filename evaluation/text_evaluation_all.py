21717310bfb9b32108e5124206dcaf66d90a5e34
mxin262 <62656382+mxin262@users.noreply.github.com>
2023-12-28 12:09:09 +0800

    fix empty dict

--- evaluation/text_evaluation_all.py
+++ evaluation/text_evaluation_all.py
@@ -84,7 +84,7 @@ class TextEvaluator():
             self._text_eval_confidence = 0.415
             weighted_ed = False
             self.submit = True
-        if "vintext" in dataset_name:
+        elif "vintext" in dataset_name:
             self.CTLABELS = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 'ˋ', 'ˊ', '﹒', 'ˀ', '˜', 'ˇ', 'ˆ', '˒', '‑']
             self.voc_size = 107
             self._text_eval_gt_path = "evaluation/gt_vintext.zip"
